99307b8ba3d2db718c990a63e949ae161e26af78
Naumovets <naumovets.daniil2003@mail.ru>
2023-07-30 17:42:07 +0800

    test

--- quantum-vue/src/views/CartView.vue
+++ quantum-vue/src/views/CartView.vue
@@ -222,7 +222,7 @@
                                 <span class="mb-2 form-label">Итого:</span>
                                 <div class="d-flex align-items-center justify-content-between">
                                     <span>{{data.value.result_price}} ₽</span>
-                                    <form name="t-payform" onsubmit="form_data=addOrderItems(); pay(form_data); return false;">
+                                    <form name="t-payform" :on-submit="pay(addOrderItems())">
                                         <button :disabled="courses.length==0" type="submit" class="btn btn-outline-primary">Оплатить</button>
                                     </form>
                                 </div>
