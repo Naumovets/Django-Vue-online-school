47d2f90c9430a29d30b822cefd7242f69eed31dc
Naumovets <naumovets.daniil2003@mail.ru>
2023-08-26 07:46:53 +0000

    update settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-81!3u)o$^&eq(#%k2g0xm5+9zt0oz9&gbtv9^4c*aq1q6e1x8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ('admin.lk-quantum.ru', '127.0.0.1')
 
